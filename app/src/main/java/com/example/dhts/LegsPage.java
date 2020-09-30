d078c2739674ce3e635a0004e44d59419c5ea54e
Thariya99 <tdilshan2525@gmail.com>
2020-09-29 23:05:25 -0700

    doumble squat 30/09/2020

---
+++ app/src/main/java/com/example/dhts/LegsPage.java
@@ -0,0 +1,37 @@
+package com.example.dhts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LegsPage extends AppCompatActivity {
+
+    Button b1, b2, b3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_legs_page);
+
+        b1 = findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),DumSquat.class);
+                startActivity(i);
+            }
+        });
+
+        b2 = findViewById(R.id.button1);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),DoumbellFlys.class);
+                startActivity(i);
+            }
+        });
+    }
+}
