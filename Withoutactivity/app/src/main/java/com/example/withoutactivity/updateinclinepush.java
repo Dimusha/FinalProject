35e3c99f06bae8ce5ef7689b59d298ebdb3fbcf1
Heshan99 <raviandrock1999@gmail.com>
2020-10-02 03:11:11 +0530

    7th commit

--- Withoutactivity/app/src/main/java/com/example/withoutactivity/updateinclinepush.java
+++ Withoutactivity/app/src/main/java/com/example/withoutactivity/updateinclinepush.java
@@ -25,57 +25,6 @@ public class updateinclinepush extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_updateinclinepush);
 
-        upinfo1 = findViewById(R.id.addi1);
-        upinfo2 = findViewById(R.id.addi2);
-        upinfo3 = findViewById(R.id.addi3);
-        update = findViewById(R.id.upbi);
 
-        String in1 = getIntent().getStringExtra("f0");
-        String in2 = getIntent().getStringExtra("f1");
-        String in3 = getIntent().getStringExtra("f2");
-
-        upinfo1.setText(in1);
-        upinfo2.setText(in2);
-        upinfo3.setText(in3);
-
-        update.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String info1 = upinfo1.getText().toString();
-                String info2 = upinfo2.getText().toString();
-                String info3 = upinfo3.getText().toString();
-
-                ref = FirebaseDatabase.getInstance().getReference().child("inclinepushups");
-
-                HashMap hashMap = new HashMap();
-
-                hashMap.put("addi1", info1);
-                hashMap.put("addi2", info2);
-                hashMap.put("addi3", info3);
-
-
-
-                ref.child("Additional info").updateChildren(hashMap).addOnSuccessListener(new OnSuccessListener() {
-
-                    String data1 = upinfo1.getText().toString();
-                    String data2 = upinfo2.getText().toString();
-                    String data3 = upinfo3.getText().toString();
-
-                    @Override
-                    public void onSuccess(Object o) {
-
-                        Toast.makeText(getApplicationContext(), "Successfully Updated", Toast.LENGTH_LONG).show();
-                        Intent i = new Intent(getApplicationContext(), InclinePushup.class);
-
-
-                        i.putExtra("f0", data1);
-                        i.putExtra("f1",data2);
-                        i.putExtra("f2",data3);
-                        startActivity(i);
-
-                    }
-                });
-            }
-        });
     }
 }
