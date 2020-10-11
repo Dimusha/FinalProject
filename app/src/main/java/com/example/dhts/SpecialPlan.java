7341018d40b4123fa04a1876d9206747fd0c7160
Dimusha <dimusha.perera@gmail.com>
2020-10-11 12:20:18 -0700

    Final decoration Commit

--- app/src/main/java/com/example/dhts/SpecialPlan.java
+++ app/src/main/java/com/example/dhts/SpecialPlan.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class SpecialPlan extends AppCompatActivity {
 
     Button button,bt,btn;
+    ImageButton btn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +20,7 @@ public class SpecialPlan extends AppCompatActivity {
         button = findViewById(R.id.btn3);
         bt = findViewById(R.id.btn10);
         btn = findViewById(R.id.btn11);
+        btn2 = findViewById(R.id.back);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,6 +43,15 @@ public class SpecialPlan extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(getApplicationContext(),category3.class);
+                startActivity(i);
+
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),home.class);
                 startActivity(i);
 
             }
